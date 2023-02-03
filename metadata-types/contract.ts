dc854781c042e2a48e998f73eb8d52b8477d1bfa
rouzwelt <rouzweltt@gmail.com>
2023-02-03 23:39:10 +0000

    update

--- metadata-types/contract.ts
+++ metadata-types/contract.ts
@@ -15,7 +15,7 @@ export type ContractMetadata = {
     /**
      * @title Contract Alias
      * @description Alias of the contract used by RainLang, follows RainLang word pattern.
-     * @pattern ^[a-z][a-z0-9]*$
+     * @pattern ^[a-z][a-z0-9-]*$
      */
     alias: string;
     /**
@@ -46,15 +46,15 @@ export type Method = {
      */
     desc: string;
     /**
-     * @title Method Expressions
-     * @minItems 1
-     */
-    expressions: Expression[];
-    /**
      * @title Method Inputs
      * @minItems 1
      */
     inputs?: Input[]
+    /**
+     * @title Method Expressions
+     * @minItems 1
+     */
+    expressions: Expression[];
 }
 
 // Additional information about inputs in this ABI.
@@ -74,128 +74,6 @@ export type Input = {
     path: string;
 }
 
-// // type of contract method arguments for metadata
-// export type Arguments = Bool | Number | String | Tuple | Array | Expression
-
-// // argument type for boolean
-// export type Bool = {
-//     /**
-//      * @title Argument Name
-//      */
-//     name: string;
-//     /**
-//      * @title Argument Description
-//      */
-//     desc: string;
-//     /**
-//      * @title Argument Path
-//      * @description Argument's path in contract's ABI, relative to parents path.
-//      */
-//     path: string;
-//     /**
-//      * @title Argument Type
-//      * @description Determines the type of the argument which can be either of string, nummber, boolean, tuple, array or exp (EvaluableConfig).
-//      */
-//     type: "boolean"
-// }
-
-// // argument type for numbers
-// export type Number = {
-//     /**
-//      * @title Argument Name
-//      */
-//     name: string;
-//     /**
-//      * @title Argument Description
-//      */
-//     desc: string;
-//     /**
-//      * @title Argument Path
-//      * @description Argument's path in contract's ABI, relative to parents path.
-//      */
-//     path: string;
-//     /**
-//      * @title Argument Type
-//      * @description Determines the type of the argument which can be either of string, nummber, boolean, tuple, array or exp (EvaluableConfig).
-//      */
-//     type: "number"
-// }
-
-// // argument type for string
-// export type String = {
-//     /**
-//      * @title Argument Name
-//      */
-//     name: string;
-//     /**
-//      * @title Argument Description
-//      */
-//     desc: string;
-//     /**
-//      * @title Argument Path
-//      * @description Argument's path in contract's ABI, relative to parents path.
-//      */
-//     path: string;
-//     /**
-//      * @title Argument Type
-//      * @description Determines the type of the argument which can be either of string, nummber, boolean, tuple, array or exp (EvaluableConfig).
-//      */
-//     type: "string"
-// }
-
-// // argument type for tuple (key/value)
-// export type Tuple = {
-//     /**
-//      * @title Argument Name
-//      */
-//     name: string;
-//     /**
-//      * @title Argument Description
-//      */
-//     desc: string;
-//     /**
-//      * @title Argument Path
-//      * @description Argument's path in contract's ABI, relative to parents path.
-//      */
-//     path: string;
-//     /**
-//      * @title Argument Type
-//      * @description Determines the type of the argument which can be either of string, nummber, boolean, tuple, array or exp (EvaluableConfig).
-//      */
-//     type: "tuple"
-//     /**
-//      * @title Tuple Properties
-//      */
-//     props: (Number | String | Tuple | Array | Expression)[]
-// }
-
-// // argument type for array
-// export type Array = {
-//     /**
-//      * @title Argument Name
-//      */
-//     name: string;
-//     /**
-//      * @title Argument Description
-//      */
-//     desc: string;
-//     /**
-//      * @title Argument Path
-//      * @description Argument's path in contract's ABI, relative to parents path.
-//      */
-//     path: string;
-//     /**
-//      * @title Argument Type
-//      * @description Determines the type of the argument which can be either of string, nummber, boolean, tuple, array or exp (EvaluableConfig).
-//      */
-//     type: "array";
-//     /**
-//      * @title Array Item Type
-//      * @description Determines the type of the items of the array.
-//      */
-//     items: Number | String | Tuple | Array | Expression
-// }
-
 // Additional information about expressions (EvaluableConfig) in this ABI.
 // argument type for expression (EvaluableConfig)
 export type Expression = {
@@ -212,16 +90,6 @@ export type Expression = {
      * @description Expression's path in contract's ABI.
      */
     path: string;
-    // /**
-    //  * @title Expression Type
-    //  * @description Determines the type of the argument which can be either of string, nummber, boolean, tuple, array or exp (EvaluableConfig).
-    //  */
-    // type: "exp"
-    /**
-     * @title Expression Context Columns
-     * @description Specifies the reserved context columns of this expression.
-     */
-    contextColumns?: ContextColumn[];
     /**
      * @title Expression Signed Context
      * @description Determines if this expression has signedContext.
@@ -232,6 +100,11 @@ export type Expression = {
      * @description Determines if this expression has callerContext.
      */
     callerContext?: boolean;
+    /**
+     * @title Expression Context Columns
+     * @description Specifies the reserved context columns of this expression.
+     */
+    contextColumns?: ContextColumn[];
 }
 
 // Each column in the context, cells are optional in the case of additional
@@ -239,19 +112,30 @@ export type Expression = {
 export type ContextColumn = {
     /**
      * @title Context Column Name
-     * @pattern ^[a-z][a-z0-9]*$
      */
     name: string;
     /**
      * @title Context Column Description
      */
-    desc: string;
+    desc?: string;
     /**
      * @title Context Column Alias
      * @description Alias for the context column used in RainLang, follows rainlang valid word pattern.
-     * @pattern ^[a-z][a-z0-9]*$
+     * @pattern ^[a-z][a-z0-9-]*$
      */
     alias: string;
+    /**
+     * @title Opcode
+     * @description Opcode to be assigned to.
+     * @pattern ^[a-z][a-z0-9-]*$
+     */
+    opcode: string;
+    /**
+     * @title Operand
+     * @description Operand to be assigned to, values inside <>.
+     * @minItems 1
+     */
+    operand: Integer[];
     /**
      * @title Context Column Cells
      * @description Info about the cells in this context column.
@@ -263,17 +147,34 @@ export type ContextColumn = {
 export type ContextCell = {
     /**
      * @title Context Cell Name
-     * @pattern ^[a-z][a-z0-9]*$
      */
     name: string;
     /**
      * @title Context Cell Description
      */
-    desc: string;
+    desc?: string;
     /**
      * @title Context Cell Alias
      * @description Alias for the context cell used in RainLang, follows rainlang valid word pattern.
-     * @pattern ^[a-z][a-z0-9]*$
+     * @pattern ^[a-z][a-z0-9-]*$
      */
     alias: string;
+    /**
+     * @title Opcode
+     * @description Opcode to be assigned to.
+     * @pattern ^[a-z][a-z0-9-]*$
+     */
+    opcode: string;
+    /**
+     * @title Operand
+     * @description Operand to be assigned to, values inside <>.
+     * @minItems 1
+     */
+    operand: Integer[];
 }
+
+/**
+ * @minimum 0
+ * @maximum 65535
+ */
+type Integer = number
